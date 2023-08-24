2fa12c9e05421a7e708265f4a616ca4130eb1ed7
kezuh1ro <128370358+kezuh1ro@users.noreply.github.com>
2023-08-24 18:35:47 +0500

    Update InformationBot.py

--- InformationBot.py
+++ InformationBot.py
@@ -1,21 +1,32 @@
 import telebot
+import asyncio
 
 bot = telebot.TeleBot('6607352864:AAEphPnsSaaa8zMXRBV097S_pNuVqCrXc4A')
 
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.send_message(message.chat.id, message)
+    bot.send_message(message.chat.id, f'Hello {message.from_user.first_name}!')
 
 @bot.message_handler(commands=['info'])
-def info(message):
-    bot.send_message(message.chat.id,
-        f"<b>First Name:</b> {message.from_user.first_name or 'None'}\n"
-        f"<b>Last Name</b>: {message.from_user.last_name or 'None'}\n"
-        f"<b>Username</b>: @{message.from_user.username or 'None'}\n"
-        f"<b>ID</b>: <code>{message.from_user.id}</code>",
-        parse_mode='html'
-    )
+async def info(message):
+    reply = await message.get_reply_message
+    if reply:
+        await bot.send_message(message.chat.id,
+            f"<b>First Name:</b> {reply.sender.first_name or 'None'}\n"
+            f"<b>Last Name</b>: {reply.sender.last_name or 'None'}\n"
+            f"<b>Username</b>: @{reply.sender.username or 'None'}\n"
+            f"<b>ID</b>: <code>{reply.sender.id}</code>",
+            parse_mode='html'
+        )
+    else:
+        await bot.send_message(message.chat.id,
+            f"<b>First Name:</b> {message.from_user.first_name or 'None'}\n"
+            f"<b>Last Name</b>: {message.from_user.last_name or 'None'}\n"
+            f"<b>Username</b>: @{message.from_user.username or 'None'}\n"
+            f"<b>ID</b>: <code>{message.from_user.id}</code>",
+            parse_mode='html'
+        )
     
     
 bot.infinity_polling()
