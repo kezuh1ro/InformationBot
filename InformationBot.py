f6cafb01dc1afd826ec11b3517853d7461eefe83
kezuh1ro <128370358+kezuh1ro@users.noreply.github.com>
2023-08-24 18:44:11 +0500

    Update InformationBot.py

--- InformationBot.py
+++ InformationBot.py
@@ -9,10 +9,10 @@ def start(message):
     bot.send_message(message.chat.id, f'Hello {message.from_user.first_name}!')
 
 @bot.message_handler(commands=['info'])
-async def info(message):
-    reply = await message.get_reply_message
+def info(message):
+    reply = message.get_reply_message
     if reply:
-        await bot.send_message(message.chat.id,
+        bot.send_message(message.chat.id,
             f"<b>First Name:</b> {reply.sender.first_name or 'None'}\n"
             f"<b>Last Name</b>: {reply.sender.last_name or 'None'}\n"
             f"<b>Username</b>: @{reply.sender.username or 'None'}\n"
@@ -20,18 +20,13 @@ async def info(message):
             parse_mode='html'
         )
     else:
-        await bot.send_message(message.chat.id,
+        bot.send_message(message.chat.id,
             f"<b>First Name:</b> {message.from_user.first_name or 'None'}\n"
             f"<b>Last Name</b>: {message.from_user.last_name or 'None'}\n"
             f"<b>Username</b>: @{message.from_user.username or 'None'}\n"
             f"<b>ID</b>: <code>{message.from_user.id}</code>",
             parse_mode='html'
         )
-        
-async def main():
-    await info()
-    
-asyncio.run(main)
 
     
 bot.infinity_polling()
