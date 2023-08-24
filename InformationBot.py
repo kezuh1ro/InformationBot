e2971b152ed3318722fb43ca19afa2e632e67d77
kezuh1ro <128370358+kezuh1ro@users.noreply.github.com>
2023-08-24 17:45:02 +0500

    Update InformationBot.py

--- InformationBot.py
+++ InformationBot.py
@@ -5,11 +5,11 @@ bot = telebot.TeleBot('6607352864:AAEphPnsSaaa8zMXRBV097S_pNuVqCrXc4A')
 
 @bot.message_handler(commands=['start'])
 def start(message):
-    bot.reply_to(message.chat.id, f'Hello {message.from_user.first_name}!')
+    bot.send_message(message.chat.id, f'Hello {message.from_user.first_name}!')
 
 @bot.message_handler(commands=['info'])
 def info(message):
-    bot.reply_to(message.chat.id,
+    bot.send_message(message.chat.id,
         f"<b>First Name:</b> {message.from_user.first_name or 'None'}\n"
         f"<b>Last Name</b>: {message.from_user.first_name or 'None'}\n"
         f"<b>Username</b>: @{message.from_user.username or 'None'}\n"
